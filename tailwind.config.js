d3a08a0c62f08094b47a63e7f0798e84445cab29
ImpuhweStella <stellahimpuhwe346@gmail.com>
2024-06-06 16:01:40 +0200

    Complete footer section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        k2d: ['K2D', 'sans-serif'],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
