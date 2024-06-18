a74bb9a7b1b65dba0a4a0a322b1275401d597ae4
ishimwepacifique0 <ishimwepacifique0@gmail.com>
2024-06-18 11:35:41 +0200

    change on routering link

--- components/Header.jsx
+++ components/Header.jsx
@@ -35,41 +35,43 @@ function Header() {
     <>
       <div className={`fixed z-50 w-full px-4 md:px-[50px] md:py-3 py-3 transition-colors duration-300 ${scrolled ? 'bg-[#111E37] bg-opacity-100' : 'bg-[#111E37] bg-opacity-0'} text-white`}>
         <div className='max-w-[86rem] mx-auto flex justify-between items-center'>
-        <div>
-          <Image src={LogoImage} width={82.21} height={60} />
+          <div>
+            <Link href={'/'} >
+              <Image src={LogoImage} width={82.21} height={60} />
+            </Link>
+          </div>
+          <div className='hidden md:flex'>
+            <ul className='w-full flex justify-between items-center  gap-[30px]  font'>
+              <Link href="/">
+                <li className='text-[17px] font-semibold'>Home</li>
+              </Link>
+              <Link href="/courses">
+                <li className='text-[17px] font-semibold'>About us</li>
+              </Link>
+              <Link href="/community">
+                <li className='text-[17px] font-semibold'>What we do</li>
+              </Link>
+              <Link href="/careers">
+                <li className='text-[17px] font-semibold'>Careers</li>
+              </Link>
+              <Link href="/news">
+                <li className='text-[17px] font-semibold'>News and Events</li>
+              </Link>
+              <Link href="/news">
+                <li className='text-[17px] font-semibold'>Our teams</li>
+              </Link>
+              <Link href="/news">
+                <li className='text-[17px] font-semibold'>Contact us</li>
+              </Link>
+            </ul>
+          </div>
+          <div className='hidden md:block'>
+            <ButtonOne text="Enroll now" />
+          </div>
+          <div className='md:hidden'>
+            <BiMenuAltRight size={30} onClick={toggleMenu} />
+          </div>
         </div>
-        <div className='hidden md:flex'>
-          <ul className='w-full flex justify-between items-center  gap-[30px]  font'>
-            <Link href="/home">
-              <li className='text-[17px] font-semibold'>Home</li>
-            </Link>
-            <Link href="/courses">
-              <li className='text-[17px] font-semibold'>About us</li>
-            </Link>
-            <Link href="/community">
-              <li className='text-[17px] font-semibold'>What we do</li>
-            </Link>
-            <Link href="/careers">
-              <li className='text-[17px] font-semibold'>Careers</li>
-            </Link>
-            <Link href="/news">
-              <li className='text-[17px] font-semibold'>News and Events</li>
-            </Link>
-            <Link href="/news">
-              <li className='text-[17px] font-semibold'>Our teams</li>
-            </Link>
-            <Link href="/news">
-              <li className='text-[17px] font-semibold'>Contact us</li>
-            </Link>
-          </ul>
-        </div>
-        <div className='hidden md:block'>
-          <ButtonOne text="Enroll now" />
-        </div>
-        <div className='md:hidden'>
-          <BiMenuAltRight size={30} onClick={toggleMenu} />
-        </div>
-      </div>
       </div>
       {menuOpen && (
         <div className='md:hidden fixed top-0 left-0 w-full h-full bg-[#011225] text-white z-40'>
