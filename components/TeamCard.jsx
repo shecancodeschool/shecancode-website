a2d8a18e315aed320b4e25a51c6db1ed9b14d583
k3lly003 <ntirukelly@gmail.com>
2024-06-23 19:45:08 +0200

    feat(add a page for shecancode team)#shec50

---
+++ components/TeamCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FiFacebook, FiInstagram, FiTwitter } from "react-icons/fi";
+import profile from "@/public/vc.jpeg";
+
+const TeamCard = ({name,title}) => {
+  return (
+    <>
+      <div className="w-full h-full flex flex-col gap-0 bg-white mb-5">
+        <div className="h-[220px] w-[100%]">
+          <Image src={profile} alt="default" className="object-cover w-[100%] h-[100%]"/>
+        </div>
+        <div className="w-full flex flex-col gap-1 pl-3">
+          <h2 className="font-bold">{name}</h2>
+          <p className="text-blue-400">{title}</p>
+        </div>
+        <div className="w-full flex gap-5 text-blue-400 pl-3 pt-3">
+          <span className="p-2 rounded-full bg-gray-100 hover:bg-blue-600 hover:text-white">
+            <Link href="#"><FiFacebook /></Link>
+          </span>
+          <span className="p-2 rounded-full bg-gray-100 hover:bg-red-400 hover:text-white">
+            <Link href="#"><FiInstagram /></Link>
+          </span>
+          <span className="p-2 rounded-full bg-gray-100 hover:bg-blue-400 hover:text-white">
+            <Link href="#"><FiTwitter /></Link>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TeamCard;
