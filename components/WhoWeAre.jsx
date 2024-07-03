03c97523f4c04ffc82562c1757ae6a931f838618
ishimwepacifique0 <ishimwepacifique0@gmail.com>
2024-07-03 14:32:49 +0200

    change on about the banner

--- components/WhoWeAre.jsx
+++ components/WhoWeAre.jsx
@@ -4,11 +4,11 @@ import { CareersBanner } from './Banner';
 const WhoWeAre = () => {
   return (
     <div className="flex justify-center mt-14 md:my-16">
-      <div className="text-center md:flex justify-start align-start">
+      {/* <div className="text-center md:flex justify-start align-start">
         <CareersBanner/>
-      </div>
-      <div className='flex flex-col justify-evenly items-center h-80'>
-        <h1 className='text-4xl text-blue-300 md:text-[60px] lg:text-[50px] xl:text-[60px] text-[20px] font-bold text-center font'>Our Services</h1>
+      </div> */}
+      <div className='flex flex-col justify-evenly items-center'>
+        <h1 className='text-4xl text-[#317ACC] md:text-[60px] lg:text-[50px] xl:text-[60px] text-[20px] font-bold text-center font'>Who we are</h1>
       </div>
 
     </div>
