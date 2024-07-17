7e86d5fcb1e05590e4476d9753a316aa6a6438bc
ImpuhweStella <stellahimpuhwe346@gmail.com>
2024-07-17 12:59:10 +0200

    fix: update and correct Privacy Policy page content and layout

--- components/PrivacyPolicyBanner.jsx
+++ components/PrivacyPolicyBanner.jsx
@@ -8,7 +8,7 @@ const PrivacyPolicyBanner = () => {
             <div className="relative h-screen md:h-[529px] flex flex-col justify-end overflow-hidden">
                 {/* <div className="absolute top-0 left-0 w-full md:h-full bg-opacity-[70%] bg-[#011225]"></div> */}
                 <Image src={privacyPolicyBanner} alt="privacyPolicy-banner" className="inset-0 h-full w-full object-cover" />
-                <div className='absolute inset-0 flex flex-col md:mt-4 mt-10 place-content-center gap-[56px] text-white p-4 md:p-0'>
+                <div className='absolute inset-0 flex flex-col md:mt-4 mt-10 place-content-center gap-[18px] text-white p-4 md:p-0'>
                     <h3 className="font-semibold text-[50px] font leading-[70px] md:text-[60px] md:leading-[72px] text-center md:w-[830px] h-[77px] mx-auto font text-[#FFFFFF]">
                         Privacy Policy
                     </h3>
