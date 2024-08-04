b00781301cd64dee0173e561d4736cefca7483be
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-08-04 13:11:12 +0200

    ft: Compete Responsive navigation bar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -54,11 +54,11 @@ const Navbar = () => {
 
         {/* Mobile nav  */}
         <div className={`
-            md:hidden bg-white absolute w-full h-full bottom-0 py-24 pl-0
+            md:hidden bg-white absolute w-full h-full bottom-0 overflow-y-scroll py-24 pl-0
             duration-500 ${open ? "left-0" : "left-[-100%]"}
           `}>
-          <div className="flex flex-col bg-white text-black w-full fixed bottom-0 top-10 py-10 px-10">
-            <Link href="/" className="py-7 inline-block" onClick={toggleOpen}>
+          <div className="flex flex-col bg-white text-black w-full fixed top-10 py-10 px-10">
+            <Link href="/" className="py-7 inline-block font-bold" onClick={toggleOpen}>
               Home
             </Link>
             <NavLinks toggleOpen={toggleOpen} />
