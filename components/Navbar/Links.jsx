51d5edf557105cad88185f43bb616c46cf748d1c
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-08-04 09:13:21 +0200

    Feature: New versatile navigation bar (Responsive + Dropdowns)

---
+++ components/Navbar/Links.jsx
@@ -0,0 +1,223 @@
+export const Links = [
+    {
+        name: 'Home',
+        link: "/"
+    },
+    {
+        name: 'Courses',
+        submenu: true,
+        sublinks: [
+            {
+                Head: "Topwear",
+                sublink: [
+                    {
+                        name: 'Topwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Bottomwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Accessories',
+                        link: "/"
+                    },
+                    {
+                        name: 'Fragrances',
+                        link: "/"
+                    },
+                    {
+                        name: 'Shoes',
+                        link: "/"
+                    },
+                ]
+            },
+            {
+                Head: "Bottomwear",
+                sublink: [
+                    {
+                        name: 'Topwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Bottomwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Accessories',
+                        link: "/"
+                    },
+                    {
+                        name: 'Fragrances',
+                        link: "/"
+                    },
+                    {
+                        name: 'Shoes',
+                        link: "/"
+                    },
+                ]
+            },
+            {
+                Head: "Shirtwear",
+                sublink: [
+                    {
+                        name: 'Topwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Bottomwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Accessories',
+                        link: "/"
+                    },
+                    {
+                        name: 'Fragrances',
+                        link: "/"
+                    },
+                    {
+                        name: 'Shoes',
+                        link: "/"
+                    },
+                ]
+            },
+            {
+                Head: "Footwear",
+                sublink: [
+                    {
+                        name: 'Topwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Bottomwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Accessories',
+                        link: "/"
+                    },
+                    {
+                        name: 'Fragrances',
+                        link: "/"
+                    },
+                    {
+                        name: 'Shoes',
+                        link: "/"
+                    },
+                ]
+            },
+        ]
+    },
+    {
+        name: 'About us',
+        link: "/",
+        submenu: true,
+        sublinks: [
+            {
+                Head: "Topwear",
+                sublink: [
+                    {
+                        name: 'Topwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Bottomwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Accessories',
+                        link: "/"
+                    },
+                    {
+                        name: 'Fragrances',
+                        link: "/"
+                    },
+                    {
+                        name: 'Shoes',
+                        link: "/"
+                    },
+                ]
+            },
+            {
+                Head: "Bottomwear",
+                sublink: [
+                    {
+                        name: 'Topwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Bottomwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Accessories',
+                        link: "/"
+                    },
+                    {
+                        name: 'Fragrances',
+                        link: "/"
+                    },
+                    {
+                        name: 'Shoes',
+                        link: "/"
+                    },
+                ]
+            },
+            {
+                Head: "Shirtwear",
+                sublink: [
+                    {
+                        name: 'Topwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Bottomwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Accessories',
+                        link: "/"
+                    },
+                    {
+                        name: 'Fragrances',
+                        link: "/"
+                    },
+                    {
+                        name: 'Shoes',
+                        link: "/"
+                    },
+                ]
+            },
+            {
+                Head: "Footwear",
+                sublink: [
+                    {
+                        name: 'Topwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Bottomwear',
+                        link: "/"
+                    },
+                    {
+                        name: 'Accessories',
+                        link: "/"
+                    },
+                    {
+                        name: 'Fragrances',
+                        link: "/"
+                    },
+                    {
+                        name: 'Shoes',
+                        link: "/"
+                    },
+                ]
+            },
+        ]
+    },
+    {
+        name: 'Blog',
+        link: "/"
+    },
+];
--- components/Navbar/NavLinks.jsx
+++ components/Navbar/NavLinks.jsx
@@ -1,55 +1,92 @@
+import Link from "next/link";
+import { Links } from "./Links";
+import { useState } from "react";
+import { MdKeyboardArrowUp } from "react-icons/md";
+import { MdKeyboardArrowDown } from "react-icons/md";
+
 const NavLinks = () => {
-    const links = [
-        {
-            name: 'Men',
-            submenu: true,
-            sublink: [
-                {
-                    Head: "Topwear",
-                    sublink: [
-                        {
-                            name: 'Topwear',
-                            link: "/"
-                        },
-                        {
-                            name: 'Bottomwear',
-                            link: "/"
-                        },
-                        {
-                            name: 'Accessories',
-                            link: "/"
-                        },
-                        {
-                            name: 'Fragrances',
-                            link: "/"
-                        },
-                        {
-                            name: 'Shoes',
-                            link: "/"
-                        },
-                    ]
-                }
-            ]
-        },
-        {
-            name: 'Women',
-            link: "/"
-        },
-        {
-            name: 'Kids',
-            link: "/"
-        },
-    ];
+    const [heading, setHeading] = useState("");
+    const [subHeading, setSubHeading] = useState("");
 
     return (
         <>
             {
-                links.map((link, index) => (
+                Links.map((link, index) => (
                     <div key={index}>
-                        <div className="px-3 text-left md:cursor-pointer">
-                            <h1 className="py-7">
+                        {/* Desktop menu  */}
+                        <div className="px-3 text-left md:cursor-pointer group">
+                            <h1
+                                className="py-7 flex justify-between items-center md:pr-0 pr-5 group"
+                                onClick={() => { heading !== link.name ? setHeading(link.name) : setHeading('') }}
+                            >
                                 {link.name}
+                                <span className="text-xl md:mt-1 md:hidden inline">
+                                    {heading === link.name ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
+                                </span>
+                                <span className="text-xl md:mt-1 md:ml-2 md:block hidden group-hover:rotate-180 group-hover:-mt-2">
+                                    <MdKeyboardArrowDown />
+                                </span>
                             </h1>
+                            {link.submenu && (
+                                <div>
+                                    <div className="absolute top-20 hidden group-hover:md:block hover:md:block">
+                                        <div className="py-3">
+                                            <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45"></div>
+                                        </div>
+                                        <div className="bg-white p-3.5 grid grid-cols-3 gap-10">
+                                            {link.sublinks.map((sublink, index) => (
+                                                <div key={index}>
+                                                    <h1 className="text-lg font-semibold">{sublink.Head}</h1>
+                                                    <div>
+                                                        {sublink.sublink.map((subsublink, subsubIndex) => (
+                                                            <li key={subsubIndex} className="text-sm text-gray-600 my-2.5">
+                                                                <Link
+                                                                    href={subsublink.link}
+                                                                    className="hover:text-blue-700"
+                                                                >
+                                                                    {subsublink.name}
+                                                                </Link>
+                                                            </li>
+                                                        ))}
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+
+                        {/* Mobile menu  */}
+                        <div className={`${heading === link.name ? 'md:hidden' : 'hidden'}`}>
+                            {/* Sublinks  */}
+                            {link.sublinks && link.sublinks.map((slinks, index) => (
+                                <div key={index}>
+                                    <div>
+                                        <h1
+                                            onClick={() => { subHeading !== slinks.Head ? setSubHeading(slinks.Head) : setSubHeading("") }}
+                                            className="py-4 pl-7 font-semibold md:pr-0 pr-5 flex justify-between items-center"
+                                        >
+                                            {slinks.Head}
+                                            <span className="text-xl md:mt-1 md:ml-2 inline">
+                                                {setHeading === slinks.name ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
+                                            </span>
+                                        </h1>
+                                        <div className={`${subHeading === slinks.Head ? "md:hidden" : "hidden"}`}>
+                                            {slinks.sublink.map((slinks, index) => (
+                                                <li key={index} className="py-3 pl-14">
+                                                    <Link
+                                                        href={slinks.link}
+                                                        className="hover:text-blue-700"
+                                                    >
+                                                        {slinks.name}
+                                                    </Link>
+                                                </li>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 ))
