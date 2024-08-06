819b02cb619c367a21ef8810c2400e6489d02f6b
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-08-06 08:47:57 +0200

    Refactoring

--- components/sections/home/CoursesSection.jsx
+++ components/sections/home/CoursesSection.jsx
@@ -5,7 +5,7 @@ const CoursesSection = () => {
     return (
         <ReusableSection background={'#e6f2ff'}>
             <h2 className="section-header">Market-aligned Courses Curated for Your Career Success</h2>
-
+            
         </ReusableSection>
     )
 }
