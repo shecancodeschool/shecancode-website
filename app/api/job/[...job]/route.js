1ce9b09ce2474d823cea85d993b09b81f6c5bac8
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-08-06 18:34:17 +0200

    Refactoring

--- app/api/job/[...job]/route.js
+++ app/api/job/[...job]/route.js
@@ -1,16 +1,16 @@
-// import { connectToDB } from "../../../../utils/database";
-// import JobModel from "../../../../models/job";
+import { connectToDB } from "../../../../utils/database";
+import JobModel from "../../../../models/job";
 
-// export const GET = async (req, res) => {
-//     const jobId = req.params.id;
-//     try {
-//         await connectToDB();
-//         const job = await JobModel.findById({ jobId });
-//         res.status(200).json({ job });
-//     } catch (error) { 
-//         return new Response("Failed to fetch jobs"+error.message, { status: 500 });
-//     }
-// }
+export const GET = async (req, res) => {
+    const jobId = req.params.id;
+    try {
+        await connectToDB();
+        const job = await JobModel.findById({ jobId });
+        res.status(200).json({ job });
+    } catch (error) { 
+        return new Response("Failed to fetch jobs"+error.message, { status: 500 });
+    }
+}
 
 /**
  * Updating a Job by Id. 
@@ -19,13 +19,13 @@
  * @param {*} res 
  * @returns 
  */
-// export const PATCH = async (req, res) => {
-//     const jobId = req.params.id;
-//     try {
-//         await connectToDB();
-//         const updatedJob = await JobModel.findByIdAndUpdate({ jobId }, req.body);
-//         res.status(200).json({ job: updatedJob });
-//     } catch (error) { 
-//         return res.status(500).json({ error: "Failed to update job: "+error.message });
-//     }
-// }
+export const PATCH = async (req, res) => {
+    const jobId = req.params.id;
+    try {
+        await connectToDB();
+        const updatedJob = await JobModel.findByIdAndUpdate({ jobId }, req.body);
+        res.status(200).json({ job: updatedJob });
+    } catch (error) { 
+        return res.status(500).json({ error: "Failed to update job: "+error.message });
+    }
+}
